6038180f12ebf3e0d52b29dd6f4b4c0e73766ba6
Jake <jsmith181997@gmail.com>
2024-02-28 12:53:09 -1000

    Add mouseover action listener to change square color

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ for (let i = 0; i < gridLength; i++) {
 
     let gridSquare = document.createElement('div');
     gridSquare.classList.add('grids');
+    gridSquare.addEventListener('mouseover', (e) => {
+
+        e.target.classList.add('hoverEffect');
+
+    });
 
 
     gridContainer.appendChild(gridSquare);
