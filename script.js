0c595d5d24c80b7298ed8c48ecef3e2842b39b63
Jake <jsmith181997@gmail.com>
2024-02-28 15:15:38 -1000

    Remove borders so that the grids flex properly and format layout

--- script.js
+++ script.js
@@ -2,14 +2,25 @@
 
 const gridContainer = document.querySelector('#gridContainer');
 
-let gridLength = 100;
+
+const gridSize = 75;
+let numberOfGrids = gridSize * gridSize;
+
+// container is 600x600px
+let CONTAINER_SIZE = 550;
+
+// dynamically calculate size
+let dynamicGrid = (CONTAINER_SIZE / gridSize);
 
 
-
-for (let i = 0; i < gridLength; i++) {
+for (let i = 0; i < numberOfGrids; i++) {
 
     let gridSquare = document.createElement('div');
     gridSquare.classList.add('grids');
+    gridSquare.style.width = dynamicGrid + 'px';
+    gridSquare.style.height = dynamicGrid + 'px';
+
+
     gridSquare.addEventListener('mouseover', (e) => {
 
         e.target.classList.add('hoverEffect');
